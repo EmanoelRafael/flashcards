10014445ce346b9460c2a06ce4662bf9b27b6e30
EmanoelRafael <emanoel.rafael@ufpe.br>
2024-10-20 18:34:31 -0300

    "adicionando botao para envio do arquivo de flashcards"

--- flashcards/app/AddFlashcardForm.tsx
+++ flashcards/app/AddFlashcardForm.tsx
@@ -108,6 +108,12 @@ export default function AddFlashcardForm() {
       <label className="block mb-2 text-sm font-bold tracking-tight text-gray-900 dark:text-white" htmlFor="file_input">Upload Arquivo de Flashcards</label>
       <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 mb-4" id="file_input" type="file">
       </input>
+      <button 
+        className="m-2 relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+         Add Flashcards File
+        </span>
+      </button>
 
     </form>
   );
