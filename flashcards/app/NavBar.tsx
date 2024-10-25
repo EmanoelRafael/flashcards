886b669ab53a7190e5e80d034b2bc5dbf011a725
EmanoelRafael <emanoel.rafael@ufpe.br>
2024-10-24 23:01:15 -0300

    "estilizando ao heading"

--- flashcards/app/NavBar.tsx
+++ flashcards/app/NavBar.tsx
@@ -2,12 +2,14 @@
 
 export default function NavBar() {
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
-    <div className="max-w-screen-xl grid items-center justify-items-center mx-auto p-4">
+    <nav className="bg-white border-b-4 border-emerald-300 dark:bg-gray-900">
+      <div className="max-w-screen-xl grid items-center justify-items-center mx-auto p-4">
         <h5 
-        className="m-2 text-5xl font-bold tracking-tight text-gray-900 dark:text-white "
-        >FLASHCARDS DE MANUCA</h5>
-    </div>
+          className="m-2 text-5xl font-bold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-emerald-300 to-emerald-200 dark:text-white drop-shadow-lg"
+        >
+          MEUS FLASHCARDS
+        </h5>
+      </div>
     </nav>
   );
 }
